f78cb8ec5f2af1cfde5976db269b31979db03c95
rudhipareek <iamrudhipareek16@gmail.com>
2025-02-22 15:21:11 +0530

    UI updated

--- sportstore/settings.py
+++ sportstore/settings.py
@@ -132,3 +132,7 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
--- sportstore/urls.py
+++ sportstore/urls.py
@@ -14,19 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path , include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path("", include('home.urls')),
     path('products/', include('products.urls')),
 ]
 
 
 
 ####################################
+
 from django.conf import settings
 from django.conf.urls.static import static
+#from home.views import home_view
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns = [    # Your other URL patterns]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

abc86c9d0e3335242f1b66bb0519f2342976a054
rudhipareek <iamrudhipareek16@gmail.com>
2025-02-23 22:02:50 +0530

    categories

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from .views import product_list , product_detail
 urlpatterns = [
     path('', product_list, name='product_list'),
     path('<int:product_id>/', product_detail, name='product_detail'),
+    path('<str:category>/', product_list, name='product_list_by_category')
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render ,get_object_or_404
 from .models import Product
 
-def product_list(request):
-    category = request.GET.get('category')  # Get category from URL params
-    if category:
-        products = Product.objects.filter(category=category)  # Filter products
-    else:
-        products = Product.objects.all()  # Show all products
 
-    return render(request, 'products/product_list.html', {'products': products})
+CATEGORY_DISPLAY_NAMES = {
+    "gym_equipment": "Gym Equipment",
+    "clothing": "Clothing",
+    "shoes": "Athletic Footwear",
+    "supplements": "Supplements",
+    "sports_gear": "Sports Gear",
+    "all-products": "All Products",
+}
+
+
+def product_list(request, category=None):
+    if category and category != "all-products":
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()  # Show all products if "All Products" is selected
+
+    readable_category = CATEGORY_DISPLAY_NAMES.get(category, "All Products")
+
+    return render(request, "products/product_list.html", {"products": products, "selected_category": readable_category})
+
 
 
 def product_detail(request, product_id):

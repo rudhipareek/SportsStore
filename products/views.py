3f8a0c35fc06fbb0f0d7fb58278c94d8044652f2
rudhipareek <iamrudhipareek16@gmail.com>
2025-02-24 09:32:08 +0530

    categories updated

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render ,get_object_or_404
 from .models import Product
+from django.db.models import Q 
 
 
 CATEGORY_DISPLAY_NAMES = {
@@ -13,14 +14,24 @@ CATEGORY_DISPLAY_NAMES = {
 
 
 def product_list(request, category=None):
+    query = request.GET.get("q")  
+    products = Product.objects.all()
+    
     if category and category != "all-products":
         products = Product.objects.filter(category=category)
-    else:
-        products = Product.objects.all()  # Show all products if "All Products" is selected
+    
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)  
+        )
 
     readable_category = CATEGORY_DISPLAY_NAMES.get(category, "All Products")
 
-    return render(request, "products/product_list.html", {"products": products, "selected_category": readable_category})
+    return render(request, "products/product_list.html", {
+        "products": products, 
+        "selected_category": readable_category,
+        "search_query": query,  
+    })
 
 
 
